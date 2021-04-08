af63c20f8fc5c4845c6ee4ddf4ac9942695cfc24
Cameron Beattie <beattiecameron99@gmail.com>
2021-04-07 17:12:58 -0700

    Movement and scrolling

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ let config = {
   type: Phaser.CANVAS,
   width: 640,
   height: 480,
-  scene: [ Menu, Play ]
+  scene: [Menu, Play]
 }
 
 let game = new Phaser.Game(config);
+
+// setting UI sizing
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+let starSpeed = 4;
+
+// reserve keyboard bindings
+let keyF, keyR, keyLEFT, keyRIGHT;
